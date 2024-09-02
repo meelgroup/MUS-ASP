d2c50652e2827c0b3fe4d359dc3c3f20af466e5c
mahi045 <mahibuet045@gmail.com>
2024-09-02 13:51:56 +0800

    edit in run_MUSASP

--- scripts/python/run_MUSASP.py
+++ scripts/python/run_MUSASP.py
@@ -2,29 +2,31 @@ import os, argparse, subprocess
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-i','--i', help='input CNF file', required=True)
-parser.add_argument('-h','--h', help='heuristics to be employed', required=False)
-parser.add_argument('-p','--p', help='python path', required=False)
+parser.add_argument('-heu','--heu', help='heuristics to be employed', default='', required=False)
+parser.add_argument('-p','--p', help='python path',  default='~/anaconda3/envs/kc/bin/python', required=False)
 args = parser.parse_args()
 python_dir = 'python'
 clingo_dir = 'clingo'
 heuristic = ''
-if args.h == None:
-    heuristic = args.h
+if args.heu == None:
+    heuristic = args.heu
 
 if args.p == None:
     python_dir = 'python'
 
-os.system('/usr/bin/time --verbose -o heu_${filename}.timeout {0} counter.py {1} {2} >> heuristic-{1}.out 2>&1'.format(python_dir, heuristic, args.i))
+os.system('/usr/bin/time --verbose -o heu_{2}.timeout {0} counter.py {1} {2} >> heuristic-{2}.out 2>&1'.format(python_dir, heuristic, args.i))
 
-out_file = open('output_{0}.out'.format(args.i))
+out_file = open('heuristic-{0}.out'.format(args.i))
 total_time = 3600
 heuristic_time = None
 for line in out_file:
     if line.startswith("Total execution (clock) time in seconds"):
         l = line.strip().split()
         heuristic_time = float(l[-1])
+    elif line.startswith("autarky size:") or line.startswith("identified MCSes:") or line.strip().startswith("Total") or line.startswith("-- Using ") or line.startswith("identified MCSes:"):
+        print(line.strip())
 
-print('Time spent in heuristic: {0}'.format(heuristic))
+print('Time spent in heuristic: {0}'.format(heuristic_time))
 
 out_file.close()
 total_time = total_time - heuristic_time # remaining time
